0885c1b89348e1ea71f775e06d84c355d81bf3fd
root <root@ip-172-31-12-19.us-east-2.compute.internal>
2023-12-06 05:02:31 +0000

    Modifying code for public

--- src/app.js
+++ src/app.js
@@ -3,10 +3,13 @@ const express = require('express');
 const body_parser = require('body-parser');
 
 const app = express();
-const port = 3059;
 
 const public_directory = path.join(__dirname, "../public"); //'/var/www/varunpmishra.com/public'
 const views_directory = path.join(__dirname, "../templates/views");
+
+const configs = require(path.join(__dirname, "./tools/configs.js"));
+
+require('dotenv').config(configs.src_path);
 
 app.use(express.static(path.join(public_directory)));
 app.use(body_parser.json());
@@ -18,6 +21,7 @@ app.get('/', (req, res) => {
 	res.render('index');
 })
 
+const port = process.env.PORT;
 app.listen(port, () => {
 	console.log(`Varunpmishra.com is up on port ${port}!`);
 })
